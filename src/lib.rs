6dce4fcdf5fc161b41fcb9b4a2551862a5187843
ArchLance <lancee_zhang@163.com>
2023-10-28 19:42:36 +0800

    [minigrep]: v1.0 minigrep finish

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use std::error::Error;
-use std::fs;
-pub struct Config<'a> {
+use std::{fs, env};
+pub struct Config {
     #[warn(dead_code)]
-    pub query : &'a String,
-    pub file_path : &'a String,
+    pub query : String,
+    pub file_path : String,
+    pub ignore_case : bool,
 }
 
-impl <'a> Config<'a> {
+impl Config {
     // #[warn(dead_code)]
     // fn new(args:&[String])->Config{
     //     let query = &args[1];
@@ -17,19 +18,88 @@ impl <'a> Config<'a> {
     // 有一点需要额外注意下，从代码惯例的角度出发，new 往往不会失败，毕竟新建一个实例没道理失败
     // 所以改成build会比较合适
     pub fn build(args:&[String]) -> Result<Config,&'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
+        let mut query :String = "".to_string();
+        let mut file_path:String = "".to_string();
+        // 获取query和file_path
+        for arg in args{
+            if &arg[0..2] == "-q" {
+                query = arg[3..].to_string();
+            }else if &arg[0..2] == "-f" {
+                file_path = arg[3..].to_string();
+            }
         }
-        let query = &args[1];
-        let file_path = &args[2];
-        
-        Ok(Config { query, file_path})
+        //错误处理
+        if query == "" {
+            return Err("query is empty");
+        }else if file_path == "" {
+            return Err("file path is empty");
+        }
+        //忽略大小写 any返回的是bool正好符合
+        let ignore_case = env::var("IGNORE_CASE").map_or_else(
+        |_| args.iter().any(|arg| {arg.to_lowercase() == "-i" || arg.to_lowercase() == "--ignore-case"}), 
+        |env_value| env_value == "1" || env_value.to_lowercase() == "true");
+        Ok(Config { query, file_path, ignore_case})
     }
 }
 
 pub fn run(config:Config) -> Result<(),Box<dyn Error>>{
     //增加错误处理
     let contents = fs::read_to_string(config.file_path)?;
-    println!("with content:\n{contents}");
+    if config.ignore_case {
+        for line in search_case_insensitive(&config.query, &contents) {
+            println!("{line}");
+        }
+    }else {
+        for line in search(&config.query, &contents) {
+            println!("{line}");
+        }
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query,content));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "RuSt";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three
+Trust me.";
+        assert_eq!(vec!["Rust:","Trust me."],search_case_insensitive(query, content));
+    }
+}
+// 找到content中存在query的行，并保存所在行
+pub fn search<'a>(query:&str,content:&'a str) -> Vec<&'a str> {
+    let mut results:Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+//大小写不敏感查询
+pub fn search_case_insensitive<'a>(query:&str,content:&'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
 }
--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,21 @@ fn main() {
     //如果 Result 是 Ok，那该方法就类似 unwrap：返回 Ok 内部的值；
     //如果是 Err，就调用闭包中的自定义代码对错误进行进一步处理
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        //迄今为止，所有的输出信息，无论 debug 还是 error 类型，都是通过 println! 宏输出到终端的标准输出( stdout )，但是对于程序来说，错误信息更适合输出到标准错误输出(stderr)。
+        //这样修改后，用户就可以选择将普通的日志类信息输出到日志文件 1，然后将错误信息输出到日志文件 2，甚至还可以输出到终端命令行。
+
+        //eprintln!使得错误信息重定向到stderr里面，正确信息执行cargo run时会被成功打印
+        eprintln!("Problem parsing arguments: {err}");
         //当 Result 包含错误时，我们不再调用 panic 让程序崩溃，而是通过 process::exit(1) 来终结进程
         //其中 1 是一个信号值(事实上非 0 值都可以)，通知调用我们程序的进程，程序是因为错误而退出的。
         process::exit(1);
     });
-    println!("Search for test {}",config.query);
-    println!("In file {}",config.file_path);
+    // println!("Search for test {}",config.query);
+    // println!("In file {}",config.file_path);
     // 代码执行时工作目录是根目录也就是minigrep目录下，而不是src目录
     // 用模式匹配处理返回的错误
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error:{e}");
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application error:{err}");
         process::exit(1);
     }
 }
